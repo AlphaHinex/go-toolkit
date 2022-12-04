8c910fc5c54bf3d0af5b968e727c29148b63a148
Alpha Hinex <AlphaHinex@gmail.com>
2022-12-04 15:55:08 +0800

    Full features of random pick

--- random-pick/random-pick.go
+++ random-pick/random-pick.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"os"
@@ -22,14 +23,24 @@ func main() {
 				Usage: "Pick n files",
 			},
 			&cli.StringFlag{
+				Name:  "t",
+				Value: "*",
+				Usage: "File type to pick",
+			},
+			&cli.StringFlag{
 				Name:  "i",
 				Value: ".",
 				Usage: "Path to pick files",
 			},
 			&cli.StringFlag{
-				Name:  "t",
-				Value: "*",
-				Usage: "File type to pick",
+				Name:  "o",
+				Value: ".",
+				Usage: "Output picked files",
+			},
+			&cli.BoolFlag{
+				Name:  "k",
+				Value: false,
+				Usage: "Keep picked files in path",
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
@@ -39,13 +50,44 @@ func main() {
 				log.Fatal(err)
 			}
 
+			output := cCtx.String("o")
+			_, err = os.Stat(output)
+			if os.IsNotExist(err) {
+				err = os.MkdirAll(output, 0777)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
 			n := cCtx.Int("n")
 			t := cCtx.String("t")
+			keep := cCtx.Bool("k")
+			fileSet := make(map[string]struct{})
 			rand.Seed(time.Now().UnixNano())
 			for i := 0; i < n; {
 				file := files[rand.Intn(len(files))]
-				if !file.IsDir() && (t == "*" || strings.HasSuffix(strings.ToLower(file.Name()), t)) {
-					fmt.Println(file.Name())
+				if !file.IsDir() && !strings.HasPrefix(file.Name(), ".") &&
+					(t == "*" || strings.HasSuffix(strings.ToLower(file.Name()), t)) {
+					if _, exist := fileSet[file.Name()]; exist {
+						continue
+					}
+					fileSet[file.Name()] = struct{}{}
+					from := path + "/" + file.Name()
+					to := output + "/" + file.Name()
+					op := "Copy"
+					if !keep {
+						err = os.Rename(from, to)
+						if err != nil {
+							log.Fatal(err)
+						}
+						op = "Move"
+					} else {
+						_, err = copyFile(from, to)
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+					fmt.Printf("%s %s to %s\n", op, path+"/"+file.Name(), output+"/"+file.Name())
 					i++
 				}
 			}
@@ -56,4 +98,19 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func copyFile(srcFile, destFile string) (int64, error) {
+	file1, err := os.Open(srcFile)
+	if err != nil {
+		return 0, err
+	}
+	file2, err := os.OpenFile(destFile, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return 0, err
+	}
+	defer file1.Close()
+	defer file2.Close()
+
+	return io.Copy(file2, file1)
 }
